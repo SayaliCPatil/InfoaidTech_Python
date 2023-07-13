d58b1815de4b41c92d9d7885945543ea078f4771
SayaliCPatil <131598192+SayaliCPatil@users.noreply.github.com>
2023-07-13 10:10:31 +0530

    Update DiceRoller.py

--- DiceRoller.py
+++ DiceRoller.py
@@ -9,7 +9,7 @@ def roll_dice():
     dice_images = []
     for _ in range(num_dice):
         dice_number = random.randint(1, 6)
-        dice_image = Image.open(f"C:/Users/sayali c. patil/Desktop/python/Dice_img/{dice_number}.gif")
+        dice_image = Image.open(f"C:/Users/sayali c. patil/Document/InfoaidTech-Python-Internship/InfoaidTech_Python/Dice_img/{dice_number}.gif")
         dice_image = dice_image.resize((100, 100), Image.LANCZOS)
         dice_images.append(ImageTk.PhotoImage(dice_image))
     
